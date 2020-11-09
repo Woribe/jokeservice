c688f4e7b3cee9ca3c2ab7cdbe95eb049fb53e6c
rune <rune.krogh.leevre@gmail.com>
2020-11-09 11:27:40 +0100

    Heroku

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
     { useNewUrlParser: true, useUnifiedTopology: true });
+    let http = require("http")
+
+    let port = process.env.PORT || 8080
+    
+    http.createServer(function(request, response) {
+        response.writeHead(200);
+        response.write("HelloWorld!");
+        response.end();}).listen(port);
+    
+        console.log("Listeningon port " + port + "...");
