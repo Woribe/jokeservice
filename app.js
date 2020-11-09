b530f14300892eb28fe9c24940e47fa1b55f1d5e
Mikkel Malmros <mikkelskovmalmros@hotmail.com>
2020-11-09 10:56:48 +0100

    App m.m.

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+// MongoDB og Mongoose setup
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://registry:dip999@ds042459.mlab.com:42459/krdo_joke_registry',
-    { useNewUrlParser: true, useUnifiedTopology: true });
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        autoIndex: true,
+        useUnifiedTopology: true
+    });
+
+// Middleware todo
+const express = require('express')
+const app = express()
+app.set('view engine', 'pug')
+
+const body = require("body-parser")
+app.use(body.json())
+app.use(body.urlencoded({ extended: false }))
+
+const config = require('./config')
+
+// Routes
+const jokeRouter = require('./routes/jokes')
+app.use('/jokes', jokeRouter)
+
+app.listen(config.PORT, () => { console.log(`Server startet på ${config.PORT}`); })
