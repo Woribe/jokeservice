f436fce629b1a7e677eddffa7b5a0190dceb4e25
Woribe <bechholm@gmail.com>
2020-11-10 13:51:20 +0100

    API complete

---
+++ controllers/joke_regstery.js
@@ -0,0 +1,73 @@
+const fetch = require('node-fetch');
+let registryUrl = 'https://krdo-joke-registry.herokuapp.com/api/services';
+
+exports.get = async function() {
+    const respons = await fetch(registryUrl);
+    if (respons.status !== 200) // OK
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
+exports.post = async function(url, objekt) {
+    const respons = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(objekt),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (respons.status !== 200) // OK
+    // if (respons.status !== 201) // Created
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
+exports.delete = async function(url, objekt) {
+    let respons = await fetch(url, {
+        method: "DELETE",
+        body: JSON.stringify(objekt),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (respons.status !== 200) // OK
+        throw new Error(respons.status);
+    return await respons.json();
+}
+
+async function mainGet(url) {
+    try {
+        let respons = await get(url);
+        console.log(respons);
+        console.log(respons.length);
+    } catch (fejl) {
+        console.log(fejl);
+    }
+}
+
+async function mainPost(url) {
+    try {
+        let respons = await post(url,
+            {
+                "name": "BabyJokes",
+                "address": "babyjokes.herokuapp.com",
+                "secret": "dødebabyer123",
+            }
+        );
+        console.log(respons);
+    } catch (fejl) {
+        console.log(fejl);
+    }
+    process.exit();
+}
+
+async function mainDelete(url) {
+    try {
+        let respons = await deLete(url,
+            {
+                // "name": "Test",
+                "address": "http://test.dk",
+                "secret": "secret password",
+            }
+        );
+        console.log(respons);
+    } catch (fejl) {
+        console.log(fejl);
+    }
+}
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,27 @@
 const express = require('express')
+const get = require('../controllers/joke_regstery')
+const nodeFetch = require('node-fetch')
 const router = express.Router()
 const controller = require('../controllers/jokes')
+const { json } = require('body-parser')
 
 router.get('/jokes', async (req, res) => {
     const jokes = await controller.getJokes()
     res.json(jokes)    
+})
+
+router.get('/othersites', async (req, res) => {
+    const services = await get.get()
+    res.json(services)
+})
+
+router.get('/otherjokes/:site', async (req, res) => {
+    try {
+        let result = await nodeFetch('https://' + req.params.site + '.herokuapp.com/api/jokes')
+        res.json(await result.json())
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 module.exports = router
