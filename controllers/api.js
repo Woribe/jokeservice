cfbd401269859c8b50e78e33087b6e59a8b55263
Mikkel Malmros <mikkelskovmalmros@hotmail.com>
2020-11-11 11:07:08 +0100

    Dropdown

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,7 @@
 const nodeFetch = require('node-fetch')
 const jokeController = require('./joke_regstery')
 
-exports.otherJokes = async function(url) {
+exports.otherJokes = async function (url) {
     try {
         let result = await nodeFetch('https://' + req.params.site + '.herokuapp.com/api/jokes')
         let json = await result.json()
@@ -9,7 +9,7 @@ exports.otherJokes = async function(url) {
     } catch (e) {
         console.log(e)
     }
-    
+
 }
 
 exports.othersjokes = async function (url) {
@@ -20,24 +20,22 @@ exports.othersjokes = async function (url) {
     } catch (e) {
         console.log(e);
     }
-    
-    
+
+
 }
 
-exports.otherSitesName = async function() {
+exports.otherSitesName = async function () {
     try {
-        const reqex = /\/([a-zA-Z0-9-_]+)/g
         let result = await jokeController.get()
-        
+
         let names = []
         for (const address of result) {
             console.log(address.address);
-            names.push({name:address.name, address:address.address})
+            names.push({ name: address.name, address: address.address })
         }
-        
         return names
     } catch (e) {
         console.log(e);
     }
-    
+
 }
