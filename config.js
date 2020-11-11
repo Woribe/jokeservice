8448469732233426113cfa85b2ff4f643262d9a4
Mikkel Malmros <mikkelskovmalmros@hotmail.com>
2020-11-11 12:54:31 +0100

    Uden kommentarer

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 let config = {}
 // MongoDB skyen
 config.mongoDBHost = 'mongodb+srv://admin:admin@skole.okeaw.mongodb.net/jokes?retryWrites=true&w=majority'
-
-//Local mongoDB
-//config.mongoDBHost = 'mongodb://localhost/jokes'
-
 config.port = 3000
 
 module.exports = config
